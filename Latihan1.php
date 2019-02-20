9989449beae8db75db0664528251301fe38d37ac
FaraNisha <42019868+FaraNisha@users.noreply.github.com>
2019-02-20 14:07:38 +0700

    Add files via upload

---
+++ Latihan1.php
@@ -0,0 +1,5 @@
+<?php
+echo "Namaku Fara Nisha";
+echo "<br>";
+echo "Aku sedang belajar PHP di SMK Telkom Malang";
+?>
---
+++ Latihan4.php
@@ -0,0 +1,8 @@
+<?php
+$nama="Fara Nisha";
+function tampil_nama(){
+  global $nama;
+  echo "Nama Saya : ".$nama;
+}
+tampil_nama()
+?>
